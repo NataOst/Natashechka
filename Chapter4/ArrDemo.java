d11c4d69757f71225fe98e5ca00b1203fcf28559
NataOst <michellelovesliza>
2017-12-02 21:20:36 +0200

    Add Children class to ArrDemo

--- Chapter4/ArrDemo.java
+++ Chapter4/ArrDemo.java
@@ -1,20 +1,50 @@
 public class ArrDemo {
 
 	public static void main(String[] args) {
-		int [] arr = new int[10];
-
+		Child [] children = new Child[10];
 		for (int i = 0; i < 10; i++) {
-			arr[i] = i + 1;
+			children[i] = new Child();
+			children[i].setName("Masha " + (i + 1));
+			children[i].setAge(i * 2);
+			children[i].setHeight(i + 2); 
 		}
 		int j = 0;
 		while (j < 10) {
-			System.out.println("The value is " + arr[j] + " The index is " + j);
+			System.out.println("The name is " + children[j].getName() + " The age is " + children[j].getAge() + " The height is " + children[j].getHeight());
 			j = j + 1;		
 		}
 		j = 9;
 		while (j > 0) {
-			System.out.println("The value is " + arr[j] + " The index is " + j);
+			System.out.println("The name is " + children[j].getName() + " The age is " + children[j].getAge() + " The height is " + children[j].getHeight());
 			j = j - 1;
 		}
+	}
+}
+
+class Child {
+
+	private String name;
+	private int age;
+	private int height;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getAge() {
+		return age;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getHeight() {
+		return height;
 	}
 }
